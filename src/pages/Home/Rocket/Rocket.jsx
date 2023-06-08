bdecfd5acc1c9b895bd8887aec11982b3a29db22
fahmeda5660 <fariha.208860@gmail.com>
2023-06-08 14:20:09 +0600

    home page responsive

--- src/pages/Home/Rocket/Rocket.jsx
+++ src/pages/Home/Rocket/Rocket.jsx
@@ -2,16 +2,16 @@ import rocketImg from "../../../assets/footer/footer-rocket.svg"
 const Rocket = () => {
   return (
     <div>
-      <div className="flex justify-center items-center gap-4">
-        <p className="text-3xl font-semibold">
-          <span className="text-purple-500 font-extrabold">Are you ready? </span>
+      <div className="md:flex lg:flex justify-center items-center gap-4 px-8">
+        <p className="text-3xl font-semibold text-center">
+          <span className="text-purple-500 font-extrabold ">Are you ready? </span>
           We are Waiting for You!
         </p>
-        <button className="px-16 py-6 text-white bg-purple-500 text-3xl font-semibold rounded-md mt-3 md:mt-0 lg:mt-0">
+        <button className=" w-full md:w-auto lg:w-auto mx-auto md:mx-0 lg:mx-0 px-16 py-6 text-white bg-purple-500 text-3xl font-semibold rounded-md mt-3 md:mt-0 lg:mt-0">
           Enroll Now
         </button>
       </div>
-      <img src={rocketImg} alt="" />
+      <img className="mt-6" src={rocketImg} alt="" />
     </div>
   );
 };
